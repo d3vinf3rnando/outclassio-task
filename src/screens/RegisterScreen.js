ccd1967cd8b4e737bd30da598b863cfe888cc456
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2024-06-05 11:54:08 +0530

    register screen update

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,17 @@
+
+import Button from '@mui/material/Button';
+
+import React from 'react'
+
+function RegisterScreen() {
+  return (
+    
+
+
+ <Button variant="contained">Hello world</Button>);
+}
+
+
+
+
+export default RegisterScreen
